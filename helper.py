a7cbc9dd8e68281af9ab566ec2d9250da066cc17
vagrant <vagrant@vagrant.vm>
2016-11-22 22:56:48 +0000

    cleaned up code. moved server functions to helper file

--- helper.py
+++ helper.py
@@ -1,9 +1,9 @@
 from model import User, UserPlant, PlantType, AlertType, Alert, connect_to_db, db
-from flask import jsonify, request
-import math
-from flask.ext.wtf import Form
-from wtforms import TextField, PasswordField
-from wtforms.validators import Required, Email
+from flask import jsonify, request, Flask
+
+
+# app = Flask(__name__)
+# connect_to_db(app)
 
 # Get all records from a table
 #######################################
@@ -20,81 +20,62 @@ def load_all_plant_types():
     
     return load_plants
 
-# add records to the database
+# Query for specific records in database
 #########################################
-def add_plants(user_id, plant_id):
-    """Search the userplant table for existing plants. If it exists, plant is left alone. If plant does not exist, it is added."""
+def search_user_plants(plant_id, user_id):
+    """Query db for user's plants """
     user_plant= UserPlant.query.filter_by(plant_id=plant_id, user_id=user_id).all()
 
-    # user_plant = db.session.query
+    return user_plant
+
+def search_user_alerts(user_plant_id):
+    """Query db for alerts specific to user's plants """
+    
+    user_alert= Alert.query.get(user_plant_id)
+
+    return user_alert
+
+def find_existing_user(email):
+    """Query db for existing users """
+
+    user = User.query.filter_by(email=email).first()
+
+    return user
 
 
-    if user_plant:
-        user_plant.plant_id == plant_id
-        flash('Plant already exists!')
-    else: 
-        user_plant = UserPlant(plant_id=plant_id, user_id=user_id)
-        flash('New plant added')
-        db.session.add(user_plant)
+# add records to the database
+#########################################
+def add_new_user(first_name, last_name, email, phone_number, password):
+    """Add new user to database. """
+
+    new_user = User(first_name=first_name, last_name=last_name, email=email, phone_number=phone_number, password=password)
+        
+    #add new user to the database
+    db.session.add(new_user)
+    db.session.commit()
+
+    return new_user
+
+def add_new_plant(user_id, plant_id):
+    """Add plant to database. """
+        
+    user_plant = UserPlant(plant_id=plant_id, user_id=user_id)
+    db.session.add(user_plant)
 
     db.session.commit()
 
     return user_plant
 
-
-def add_alerts(alert_type_id, user_plant_id, date):
-    """Search the alert table for existing alert. If it exists, alert is left alone. If alert does not exist, it is added."""
-
-    user_alert = Alert.query.filter_by(alert_type_id=alert_type_id, user_plant_id=user_plant_id, date=date ).all()
-
-    if user_alert:
-        flash('Alert already exists!')
-    else: 
-        user_alert = Alert(user_plant_id=user_plant_id, alert_type_id=alert_type_id, date=date)
-        flash('New alert added')
-        db.session.add(user_alert)
-
+def add_new_alert(user_plant_id, alert_type_id, date):
+    """Add alert to database. """
+    user_alert = Alert(user_plant_id=user_plant_id, alert_type_id=alert_type_id, date=date)
+    db.session.add(user_alert)
     db.session.commit()
 
     return user_alert
 
-def add_users(first_name, last_name, email, phone_number, city, state, zip_code, password):
-    """search the user table for existing user. If they exist, user is left alone. If user does not exist, they are added."""
-    new_user = User(first_name=first_name, last_name=last_name, email=email, phone_number=phone_number, city=city, state=state, zip_code=zip_code, password=password)
-
-    if new_user:
-        flash('User already exists! Please check your username or password and try again.')
-    else: 
-        new_user = User(first_name=first_name, last_name=last_name, email=email, phone_number=phone_number, city=city, state=state, zip_code=zip_code, password=password)
-        flash('New user added')
-        db.session.add(new_user)
-
-    db.session.commit()
-
-    return new_user
-
-def get_alert_type_names():
-    alert_type_names = db.session.query(AlertType.alert_type, Alert.date).join(Alert).all()
-
-    for alert_type, date in alert_type_names:
-        print alert_type, date
 
 
 
-def search_plants(user_id, plant_id):
-    """Given a user and a supply id, find an existing item record."""
-
-    results = Item.query.filter(UserPlant.user_id == user_id, UserPlant.plant_id == plant_id)
-
-    user_plant_results = results.first()
-
-    return item_from_db
-
-class EmailPasswordForm(Form):
-    email = TextField('Email', validators=[Required(), Email()])
-    password = PasswordField('Password', validators=[Required()])
 
 
-def check_if_user_logged_in():
-    user_id = session.get('user_id')
-    
--- server.py
+++ server.py
@@ -3,38 +3,26 @@
 from jinja2 import StrictUndefined
 
 from datetime import datetime
-from flask import Flask, jsonify,render_template, redirect, request, flash, session, json, url_for
-from werkzeug.utils import secure_filename
+from flask import Flask, jsonify,render_template, redirect, request, flash, session, json
 from flask_debugtoolbar import DebugToolbarExtension
 
 from model import User, UserPlant, PlantType, AlertType, Alert, connect_to_db, db
 
-from helper import load_all_plant_types, load_all_alerts_types
+from helper import load_all_plant_types, load_all_alerts_types, add_new_plant, search_user_plants, find_existing_user, add_new_user, search_user_alerts, add_new_alert
 
-UPLOAD_FOLDER = '../static/'
-ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 # Required to use Flask sessions and the debug toolbar
 app.secret_key = "ABC"
-
-# Normally, if you use an undefined variable in Jinja2, it fails
-# silently. This is horrible. Fix this so that, instead, it raises an
-# error.
 
 app.jinja_env.undefined = StrictUndefined
 app.jinja_env.auto_reload = True
 
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
 @app.route('/')
 def index():
-    """Homepage."""
+    """Display homepage."""
     
-    #display homepage
     return render_template("homepage.html")
 
 
@@ -47,7 +35,7 @@ def confirm():
     password = request.form["password"]
 
     #query User table to see if new user is actually an existing user
-    user = User.query.filter_by(email=email).first()
+    user = find_existing_user(email)
 
     #conditional to check to see if the user exists. 
     if not user:
@@ -66,10 +54,10 @@ def confirm():
 
 @app.route('/register', methods=['GET'])
 def user_signin():
-    """User sign in form."""
+    """Display registration form"""
 
-    #display registration form
     return render_template('user_form.html')
+
 
 @app.route('/register',methods=['POST'])
 def register_process():
@@ -83,18 +71,14 @@ def register_process():
     password = request.form["password"].encode('utf-8')
 
     #query User table to see if new user is actually an existing user
-    new_user = User.query.filter_by(email=email).first()
+    new_user = find_existing_user(email)
 
-    #conditional to check if user exists in db. If user exists, redirect to login. Otherwise add to db and redirect to user's profile page.
+    #If user exists, redirect to login. Otherwise add to db and redirect to user's profile page.
     if new_user:
         flash('You already exist! Please log in.')
         return redirect('/login')
     else: 
-        new_user = User(first_name=first_name, last_name=last_name, email=email, phone_number=phone_number, password=password)
-        
-    #add new user to the database
-        db.session.add(new_user)
-    db.session.commit()
+        new_user = add_new_user(first_name, last_name, email, phone_number, password)
 
     flash("User %s added." % email)
     session["user_id"] = new_user.user_id
@@ -128,27 +112,23 @@ def add_plants():
 
     #conditional to verify user is logged in before plants are added.
     if user_id:
-        user_plant = UserPlant.query.filter_by(user_id=user_id, plant_id=plant_id).first()
+        user_plant = search_user_plants(plant_id, user_id)
 
     else:
         raise Exception("No user logged in.")  
     
-    #conditional that searches userplant table for existing plants. if it already exists, plant is left alone. if plant does not exist, it's added to the table.
+    #If user's plant exists, do nothing. Otherwise add to db and redirect to user's profile page.
     if user_plant:
         flash('Plant already exists!')
     else: 
-        user_plant = UserPlant(plant_id=plant_id, user_id=user_id)
+        add_new_plant(user_id, plant_id)
         flash('New plant added')
-        db.session.add(user_plant)
-
-    db.session.commit()
-
 
     return redirect("/users/" + str(user_id))
 
+
 @app.route('/addalerts', methods=['POST'])
 def add_alerts():
-
     """Users can add alerts to the plants on their profiles. """
 
     #get form variables
@@ -158,22 +138,17 @@ def add_alerts():
     user_id = session.get('user_id')
 
     #conditional to verify user is logged in before alerts are added.
-
     if user_id:
-        user_alert = Alert.query.filter_by(alert_type_id=alert_type_id, user_plant_id=user_plant_id, date=date ).all()
-
+        user_alert = search_user_alerts(alert_type_id)
     else:
         raise Exception("No user logged in.")  
 
-    #conditional that searches userplant table for existing plants. if it already exists, plant is left alone. if plant does not exist, it's added to the table.
+    #If user's alert exists, do nothing. Otherwise add to db and redirect to user's profile page.
     if user_alert:
         flash('Alert updated!')
     else: 
-        user_alert = Alert(user_plant_id=user_plant_id, alert_type_id=alert_type_id, date=date)
+        add_new_alert(user_plant_id, alert_type_id, date)
         flash('New alert added')
-        db.session.add(user_alert)
-
-    db.session.commit()
     
     return redirect("/users/" + str(user_id))
 
@@ -191,15 +166,14 @@ def add_qty():
     if user_id:
         plant_number = UserPlant.query.get(user_plant_id)
         plant_number.qty = qty
-
     else:
         raise Exception("No user logged in.")  
-
 
     flash('Quantity updated')
     db.session.commit()
 
     return jsonify({'user_plant_id':user_plant_id, 'qty':qty})
+
 
 @app.route('/completealert.json', methods=['POST'])
 def complete_alert():
@@ -214,30 +188,29 @@ def complete_alert():
     if user_id:
         alert_completion = Alert.query.get(user_plant_id)
         alert_completion.completion = completion
-
     else:
         raise Exception("No user logged in.")  
 
-
     flash('Task status updated')
-    # db.session.add(plant_number)
     db.session.commit()
 
     return jsonify({'completion':completion, 'user_plant_id':user_plant_id})
 
+
 @app.route('/logout')
 def logout():
     """Log out."""
+
     #delete the session user_id
     del session["user_id"]
     flash("Logged Out.")
+
     #return user ot the homepage
     return redirect("/")
 
 
 if __name__ == "__main__":
-    # We have to set debug=True here, since it has to be True at the
-    # point that we invoke the DebugToolbarExtension
+
     app.debug = True
 
     connect_to_db(app)
@@ -245,6 +218,4 @@ if __name__ == "__main__":
     # Use the DebugToolbar
     DebugToolbarExtension(app)
 
-
-    
     app.run(host='0.0.0.0', port=5000)
